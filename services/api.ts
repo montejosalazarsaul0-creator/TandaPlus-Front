6c8d1a8f707898aace72acfb55d08927f9d19600
Saul Montejo <montejosalazarsaul0@gmail.com>
2026-05-17 03:52:45 -0600

    fix: baseURL apuntando al Render

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const api = axios.create({
-  baseURL: "http://192.168.1.66:3000"
+  baseURL: "https://tandaplus-backend.onrender.com",
   //baseURL: "http://localhost:3000"
 });
 
